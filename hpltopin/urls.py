d4bea6b594565d03621b2e66d26731d874be39bf
Kevin Andrews <andrewskevin92@gmail.com>
2020-02-05 06:34:51 -0800

    add projects to homepage

--- hpltopin/urls.py
+++ hpltopin/urls.py
@@ -4,7 +4,7 @@ from my_site import giphy
 
 
 urlpatterns = [
-    path('', views.index, name='index'),   
+    path('', views.index, name='hpltopin-home'),   
     path('get_listings/', views.get_listings, name='get_listings'),
     path('pin_list/', views.create_and_post, name='pin_list'),    
 ]
